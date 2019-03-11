c248e55263f11e0a539bba7e6c6fc06137982325
Adeel Nayyer Sheikh <adeelnayyersheikh@gmail.com>
2019-03-11 08:05:14 +0400

    CSS processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 const watch = require('gulp-watch');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
 
 gulp.task('default', () => {
   console.log('default');
@@ -9,12 +13,18 @@ gulp.task('html', () => {
   console.log('html');
 });
 
-gulp.task('html', () => {
-  console.log('html');
+gulp.task('styles', () => {
+  return gulp.src('./app/assets/styles/site.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/dist/assets/styles'));
 });
 
 gulp.task('watch', () => {
   watch('./app/index.html', () => {
     gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('styles');
   });
 });
